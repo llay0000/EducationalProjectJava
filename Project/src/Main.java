c052e5fc4cda8a74a7897a1ffb9395f4154cea87
llay000 <ilay2018rassi@gmail.com>
2023-09-21 15:22:13 +0300

    Проверка

---
+++ Project/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        int num;
+        int num2;
+        for (int i = 0; i <= 10; i++) {
+            num =  (int)(Math.random()*100);
+            if () {
+
+            }
+
+        }
+        System.out.println("Hello world!");
+    }
+}
