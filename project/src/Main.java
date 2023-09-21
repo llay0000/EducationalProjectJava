9fd0dbaa6f065a763fea54dd4cf8f055b0586633
llay000 <ilay2018rassi@gmail.com>
2023-09-22 00:04:42 +0300

    run

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        int num;
+        int num2;
+        for (int i = 0; i <= 10; i++) {
+            num =  (int)(Math.random()*100);
+        }
         System.out.println("Hello world!");
     }
 }
